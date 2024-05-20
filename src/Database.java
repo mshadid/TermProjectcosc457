279e4c401d316b1b03b935c5966bb6051f772090
Gavin Trevillian <47387815+GodfreyDev@users.noreply.github.com>
2024-05-20 12:22:48 -0400

    Added .gitignore and .env

--- src/Database.java
+++ src/Database.java
@@ -3,11 +3,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/gavinDB";
-    private static final String USER = "gavins username";
-    private static final String PASSWORD = "gavins password";
-
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String dbHost = System.getenv("DB_HOST");
+        String dbPort = System.getenv("DB_PORT");
+        String dbUser = System.getenv("DB_USER");
+        String dbPassword = System.getenv("DB_PASSWORD");
+        String dbName = System.getenv("DB_NAME");
+
+        String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
+        return DriverManager.getConnection(url, dbUser, dbPassword);
     }
 }
